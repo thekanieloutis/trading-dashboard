26f2691dd94d59b964d3a95c44686802602a9980
thekanieloutis <ing.yojanhernandez@gmail.com>
2026-05-12 01:27:35 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Este archivo contiene tus llaves privadas. ¡No las compartas!
+const CONFIG = {
+    APPS_SCRIPT_URL: "TU_URL_DE_GOOGLE_SHEETS_AQUÍ",
+    GEMINI_API_KEY: "gen-lang-client-0858900635"
+};
