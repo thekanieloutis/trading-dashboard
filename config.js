15641e0de2b9b85db52d307daf86d8ec43508953
thekanieloutis <ing.yojanhernandez@gmail.com>
2026-05-12 01:28:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Este archivo contiene tus llaves privadas. ¡No las compartas!
 const CONFIG = {
-    APPS_SCRIPT_URL: "TU_URL_DE_GOOGLE_SHEETS_AQUÍ",
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwIzl_Hc_TIfNodaThHIRexJz4-Ktv_H-vUyPxhZcF0vbi7gr7IsUM56-7BtGlQ9w/exec",
     GEMINI_API_KEY: "gen-lang-client-0858900635"
 };
